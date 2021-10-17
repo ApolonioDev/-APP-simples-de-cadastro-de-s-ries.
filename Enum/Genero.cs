b1a6a17fbfeb35262dcc97115e88a32ccb9e3909
Apolonio <apolonio.dev@gmail.com>
2021-10-16 21:26:29 -0300

    Primeiro commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,30 @@
+namespace DIO.Series
+{
+
+    public enum Genero
+    {
+        Acao = 1,
+        
+        Aventura = 2,
+
+        Comida = 3,
+
+        Documentacao = 4,
+
+        Drama = 5,
+
+        Espionagem = 6,
+
+        Faroeste = 7,
+
+        Ficcao_Cientifica = 9,
+
+        Musical = 10,
+
+        Romance = 11,
+
+        Suspense = 12,
+
+        Terror = 13
+    }
+}
